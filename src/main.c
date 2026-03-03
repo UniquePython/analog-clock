953f1b2512005be230dbdd297f65092a228e1a21
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-03 11:24:40 +0530

    Basic raylib setup

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include <raylib.h>
+#include <stdlib.h>
 
+#define WIDTH 900
+#define HEIGHT 600
 
 int main(void)
 {
-	printf("Hello World!\n");
-	return 0;
+	InitWindow(WIDTH, HEIGHT, "Analog Clock");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return EXIT_SUCCESS;
 }
